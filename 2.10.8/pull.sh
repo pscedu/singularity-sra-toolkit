6a7541dc5a1904a55272b93c9ffe384199a3c78c
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2020-11-06 15:14:31 -0500

    Initial untested commit

---
+++ 2.10.8/pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+singularity pull https://depot.galaxyproject.org/singularity/sra-tools%3A2.10.8--pl526haddd2b5_0
+mv sra-tools%3A2.10.8--pl526haddd2b5_0 singularity-sra-tools-2.10.8.sif
