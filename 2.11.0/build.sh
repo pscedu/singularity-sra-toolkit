54f02f1ee99c5cc4f37ef4c6986266c9a2b1bae4
halbakri <84340692+halbakri@users.noreply.github.com>
2021-08-13 12:59:37 -0400

    Update build.sh

--- 2.11.0/build.sh
+++ 2.11.0/build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-IMAGE=singularity-sra-toolkit-2.11.0.sif
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+PACKAGE=graphviz
+VERSION=2.11.0
+IMAGE=singularity-$PACKAGE-$VERSION.sif
 DEFINITION=Singularity
 
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
 sudo singularity build $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
